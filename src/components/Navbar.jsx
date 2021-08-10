89bed38fc482864c1347ae7ca1bf815a65fb832f
Nadine Coelho <nadinecoelho9@gmail.com>
2021-08-10 14:37:04 -0300

    add components to the app

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <div>
+      <nav>
+        <h1>Context App</h1>
+        <ul>
+          <li>Home</li>
+          <li>About</li>
+          <li>Contact</li>
+        </ul>
+      </nav>
+    </div>
+  );
+};
+
+export default Navbar;
